f5d63a3eff83229cd8a597dcc401a0fc7d5737e5
Kayantao <sirpendrake@gmail.com>
2016-08-30 14:35:54 +0200

    Add files via upload

---
+++ common/data/details.js
@@ -0,0 +1,860 @@
+window.details = {
+	1: {
+		ninjutsu: "2.2x Heart Damage to all enemies in range",
+		ninjutsuName: "Evil Wind Shuriken: Shadow Windmill",
+		fieldSkill: "Boosts attack by 65-130",
+		buddySkill: "Boosts attack by 50"
+	},
+	2: {
+		ninjutsu: "2.2x Heart Damage to all enemies in range",
+		ninjutsuName: "Evil Wind Shuriken: Shadow Windmill",
+		fieldSkill: "Boosts attack by 70-140",
+		buddySkill: "Boosts attack by 70"
+	},
+	3: {
+		ninjutsu: "2.2x Heart Damage to all enemies in range",
+		ninjutsuName: "Evil Wind Shuriken: Shadow Windmill",
+		fieldSkill: "Boosts attack by 75-150",
+		buddySkill: "Boosts attack by 95"
+	},
+	4: {
+		ninjutsu: "2.2 Skill Damage to all enemies in range, 25% chance of slip damage for 5 turns",
+		ninjutsuName: "Fire Style: Phoenix Flower Jutsu",
+		fieldSkill: "Boosts critical rate by 1.1x - 1.6x",
+		buddySkill: "Reduces the chance of receiving slip damage by 10%(Not Friend Skills)"
+	},
+	5: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	6: {
+		ninjutsu: "Gives you 3 perfect dodges for 3 turns",
+		ninjutsuName: "Clone Jutsu",
+		fieldSkill: "Restores 100-150 hp",
+		buddySkill: "Reduces the chance of your attack damage being reduced by 10% (Not Friend Skill)"
+	},
+	7: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	8: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	9: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	10: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	11: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	12: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	13: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	14: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	15: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	16: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	17: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	18: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	19:{
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	20: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	21: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	22: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	23: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	24: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	25: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	26:{
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	27: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	28: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	29: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	30: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	31: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	32: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	33: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	34: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	35: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	36: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	37: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	38: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	39: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	40: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	41: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	42: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	43: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	44: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	45: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	46: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	47: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	48: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	49: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	50: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	51: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	52: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	53: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	54: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	55: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	56: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	57: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	58: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	59: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	60: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	61: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	62: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	63: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	64: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	65: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	66: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	67: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	68: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	69: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	70: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	71: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	72: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	73: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	74: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	75: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	76: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	77: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	119: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	120: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	121: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	122: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	123: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	124: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	125: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	126: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	127: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	128: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	129: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	130: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	131: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	132: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	133: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	134: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	135: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	136: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	137: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	138: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	139: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	140: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	141: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	142: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	143: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	144: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	145: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	146: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	147: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	148: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	149: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	150: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	151: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	152: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	153: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	154: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	155: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	156: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	157: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	158: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	159: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	160: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	161: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	162: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	163: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	164: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	165: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	166: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	167: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	168: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	169: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	170: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	171: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	172: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	173: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	174: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	175: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	176: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	177: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	178: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	179: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	180: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	181: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	182: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	183: { 
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	},
+	184: {
+		ninjutsu: "",
+		ninjutsuName: "",
+		fieldSkill: "",
+		buddySkill: ""
+	}
+};
